9700cce252ad80817fa7bcbc0f66fd8e9a16dc15
Valeriy <dobrotskiyvd@gmail.com>
2023-09-07 17:55:28 +0300

    Added animations for running from side to side

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _runningSpeed = 1f;
-    [SerializeField] private float _sideSpeed = 0.5f;
 
     [SerializeField] private MySlider _movementSlider;
 
@@ -19,9 +17,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        //temporary
         if (!_isRunning)
             if (Input.GetMouseButtonDown(1))
                 _isRunning = true;
+        //temporary
 
         if (_movementSlider.IsHeld && _movementSlider.value != 0)
             _runningToTheSide = true;
@@ -37,17 +37,34 @@ public class PlayerMovement : MonoBehaviour
             return;
 
         if (!_runningToTheSide)
+        {
+            ResetMovingToSidesAnimations();
             _animator.SetBool("IsRunning", _isRunning);
+        }
         else
         {
             transform.Translate(transform.right * Time.deltaTime * _movementSlider.value);
 
             if (_movementSlider.value < 0)
-            {//running right anim 
+            {
+                if (_animator.GetBool("ToTheRight"))
+                    _animator.SetBool("ToTheRight", false);
+                _animator.SetBool("ToTheLeft", true);
             }
             else if (_movementSlider.value > 0)
-            {//running left anim
+            {
+                if (_animator.GetBool("ToTheLeft"))
+                    _animator.SetBool("ToTheLeft", false);
+                _animator.SetBool("ToTheRight", true);
             }
         }
+    }
+
+    private void ResetMovingToSidesAnimations()
+    {
+        if (_animator.GetBool("ToTheLeft"))
+            _animator.SetBool("ToTheLeft", false);
+        if (_animator.GetBool("ToTheRight"))
+            _animator.SetBool("ToTheRight", false);
     }
 }
